fa5cc21ecd2fc888c3d129b8d38962204b80b1ff
rajpalchandrawat <42087145+rajpalchandrawat@users.noreply.github.com>
2021-01-22 11:47:56 +0530

    Update conftest.py

--- tests/step_defs/conftest.py
+++ tests/step_defs/conftest.py
@@ -12,7 +12,7 @@ DUCKDUCKGO_HOME = 'https://duckduckgo.com/'
 # Hooks
 
 def pytest_bdd_step_error(request, feature, scenario, step, step_func, step_func_args, exception):
-    print(f'Step failed: {step}')
+    #print(f'Step failed: {step}')
 
 
 # Fixtures
